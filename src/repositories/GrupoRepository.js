579ccd8515c6c76ad9d4ebcd896f21491e068a96
Becky2710 <rebecaperez@gmail.com>
2023-10-15 00:17:20 -0300

    Mantenimiento de Grupos

---
+++ src/repositories/GrupoRepository.js
@@ -0,0 +1,59 @@
+const pool = require('../config/databaseController');
+
+module.exports = {
+
+    // Consulta para obtener todos los grupos
+    obtenerTodosLosGrupos: async() => {
+      console.log('Hizo la consulta');
+        try {
+            const result = await pool.query('SELECT a.idgrupo, a.num_grupo,a.anio,a.ciclo,b.materia,c.nombre FROM grupos a,materias b,profesores c WHERE a.idmateria = b.idmateria and a.idprofesor = c.idprofesor;');
+            return result;
+        } catch (error) {
+            console.error('Ocurrio un problema al consultar la lista de grupos: ', error);
+        }
+    },
+               
+
+     // Insertar grupo
+     insertarGrupo: async(nuevoGrupo) => {
+      try {
+          const result = await pool.query('INSERT INTO grupos SET ?', nuevoGrupo);
+          return result.insertId;
+      } catch (error) {
+          console.error('Ocurrio un problema al insertar grupo ', error);
+      }
+  },
+
+     // Actualiar grupo
+     actualizarGrupo: async(idgrupo, datosModificados) => {
+      try{
+        const result = await pool.query('UPDATE grupos SET ? WHERE idgrupo = ?', [datosModificados,idgrupo]);
+        return result.affectedRows > 0;
+      }catch(error){
+        console.error('Error al actualizar el registro', error);
+      }
+  },
+
+  
+
+     // Obtener grupo por ID
+     obtenerGrupoPorid: async (idgrupo) =>{
+      try {
+          const[grupo] = await pool.query('SELECT * FROM grupos  WHERE idgrupo = ?',[idgrupo]);
+          return grupo;
+      } catch (error) {
+         console.log('Error para obtener el registro');
+      }
+     },
+
+   // Eliminar grupo
+    eliminarGrupo: async(idgrupo) => {
+        try{
+          const result = await pool.query('DELETE FROM grupos WHERE idgrupo = ?', [idgrupo]);
+          return result.affectedRows > 0;
+        }catch(error){
+          console.error('Erro al eliminar grupo', error);
+        }
+    }
+}
+
---
+++ src/routes/grupos.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const queries = require('../repositories/GrupoRepository');
+const materiasQuery = require('../repositories/MateriaRepository');
+const profesoresQuery = require('../repositories/ProfesorRepository');
+
+// Endpoint para mostrar todos los grupos
+
+router.get('/', async (request, response) => {
+  console.log('Entro aqui');
+    const grupos = await queries.obtenerTodosLosGrupos();
+
+     response.render('grupos/listado', {grupos}); // Mostramos el listado de grupos
+});
+
+// Endpoint que permite mostrar el formulario para agregar un nuevo grupo
+
+router.get('/agregar', async(request, response) => {
+    const lstMaterias = await materiasQuery.obtenerTodasLasMaterias();
+    const lstProfesores = await profesoresQuery.obtenerTodosLosProfesores();
+
+    // Renderizamos el formulario
+    response.render('grupos/agregar', {lstMaterias,lstProfesores});
+});
+
+// Endpoint para agregar un grupo
+router.post('/agregar', async(request, response) => {
+
+    const { num_grupo, anio,ciclo, idmateria, idprofesor } = request.body;
+    const nuevoGrupo = { num_grupo, anio,ciclo, idmateria, idprofesor};
+
+    const resultado = await queries.insertarGrupo(nuevoGrupo);
+
+    response.redirect('/grupos');
+});
+
+// Endpoint que permite eliminar un grupo
+router.get('/eliminar/:idgrupo', async(request, response) => {
+
+    // Desestructuramos el objeto que nos mandan en la peticion y extraemos el idestudiante
+    const { idgrupo } = request.params;
+    const resultado = await queries.eliminarGrupo(idgrupo);
+    if(resultado > 0){
+        console.log('Eliminado con éxito');
+    }
+    response.redirect('/grupos');
+});
+
+// Endpoint para mostrar el formulario de edición
+router.get('/editar/:idgrupo', async (request, response) => {
+      const { idgrupo } = request.params;
+      const grupo = await queries.obtenerGrupoPorid(idgrupo);
+
+      if (grupo) {
+        const lstMaterias = await materiasQuery.obtenerTodasLasMaterias();
+        const lstProfesores = await profesoresQuery.obtenerTodosLosProfesores();
+        response.render('grupos/editar',{lstMaterias,lstProfesores,idgrupo,grupo});
+      }else{
+        response.redirect('/grupos');
+      }
+
+    });
+
+// Endpoint que permite editar un grupo
+router.post('/editar/:id', async (request, response) => {
+  const { id } = request.params;
+  const {num_grupo, anio,ciclo, idmateria, idprofesor} = request.body;
+  const datosModificados = {num_grupo, anio,ciclo, idmateria, idprofesor};
+
+  const resultado = await queries.actualizarGrupo(id, datosModificados);
+
+  if(resultado){
+    console.log('Grupos modificado con exito');
+    response.redirect('/grupos');
+  }else{
+    console.log('Error al modificar grupos');
+    response.redirect('/grupos/editar/'+ idgrupo);
+  }
+});
+
+
+module.exports = router;
